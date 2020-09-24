7f60518ef71a4a7356b9123b8ca111e87884efc2
meTheLearner <togunchan@gmail.com>
2020-09-25 00:46:47 +0300

    Lesson 3: Component_Templates

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { Movie } from '../movie';
 
 @Component({
     selector: 'movies', // <movies></movies>
     templateUrl: 'movies.component.html'
 })
 export class MoviesComponent {
+    title = 'Movie List'
 
+    //obje oluşturuyoruz
+    movie: Movie = {
+        id: 1,
+        name: 'Movie Name'
+
+    }
+
+    getTitle() {
+        return this.title;
+    }
 }
