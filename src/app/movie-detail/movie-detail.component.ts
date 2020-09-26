7603d7b6307944a17219570b73da3f9cbe8bcae1
meTheLearner <togunchan@gmail.com>
2020-09-27 00:18:40 +0300

    Lesson 12: Creating Movie Detail

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
 
 
 @Component({
@@ -8,12 +10,22 @@ import { Movie } from '../movie';
   styleUrls: ['./movie-detail.component.css']
 })
 export class MovieDetailComponent implements OnInit {
-
+  // localhost:4200/detail/2
   @Input() movie: Movie
 
-  constructor() { }
+  constructor(
+    private movieService: MovieService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.getMovie();
+  }
+
+  getMovie(): void {
+    //route üzerindeki id parametresine ulaşmak
+    const id = +this.route.snapshot.paramMap.get('id'); //+ ile değer number yapılır
+    this.movieService.getMovie(id).subscribe(movie=> this.movie=movie);
   }
 
 }
