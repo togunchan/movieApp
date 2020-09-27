bf4eb55d796f09bc14befc8d2739e7365e7dea03
meTheLearner <togunchan@gmail.com>
2020-09-27 18:46:59 +0300

    Lesson 13: Http Request

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -3,22 +3,28 @@ import { Movie } from './movie';
 import { Movies } from './movie.datasource';
 import {Observable, of} from 'rxjs'
 import { LoggingService } from './logging.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ /*Decoretor*/
   providedIn: 'root' //root modülü yani App modülü içinde herhangi bir yerden ulaşılabiir.
 })
 export class MovieService {
 
-  constructor(private loggingService: LoggingService) { }
+  private apiMovieUrl = 'api/movies';
+
+  constructor(
+    private loggingService: LoggingService,
+    private http: HttpClient
+    ) { }
 
   getMovies(): Observable<Movie[]> {
     this.loggingService.add('MovieService: listing movies');
-    return of(Movies);
+    return this.http.get<Movie[]>(this.apiMovieUrl);
   }
 
   getMovie(id): Observable<Movie>{
     this.loggingService.add('MovieService: get detail by id'+ id);
-    return of(Movies.find(movie=>movie.id===id));
+    return this.http.get<Movie>(this.apiMovieUrl+'/'+id);
   }
 
 
